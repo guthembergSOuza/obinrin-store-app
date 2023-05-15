e5457e5ee814ef663a059e435b4e4ca675cce7e8
T088836574 <T088836574@cassi.com.br>
2023-05-15 16:21:59 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './img/artelie.jpg';
+import './App.css';
+
+function openInstagram(){
+  window.open("https://www.instagram.com/artelieobinrin/", '_blank').focus();
+}
+
+function openWhatsapp(){
+  return window.open("https://web.whatsapp.com/send/?phone=5581995480060&text", '_blank');
+}
+
+
+function App() {
+  return (
+    <div >
+      <header className="App-header">
+        <img src={logo} className="ImgLogo"/>
+      </header>
+      <div className="App-body">
+        <div className="container">
+          <div className="box">
+            <div className="col">
+              <br/>
+              <div className="item" >Galeria</div>
+              <br/>
+              <div className="item" onClick={openInstagram}>Instagram</div>
+              <br/>
+              <div className="item" onClick={openWhatsapp}>WhatsApp</div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+
+}
+
+
+
+export default App;
---
+++ src/acoes.js
@@ -0,0 +1,8 @@
+
+  function openInstagram(){
+    window.open("https://www.instagram.com/artelieobinrin/", '_blank').focus();
+  }
+
+  function openWhatsapp(){
+    return window.open("https://contate.me/artelieobinrin", '_blank');
+  }
